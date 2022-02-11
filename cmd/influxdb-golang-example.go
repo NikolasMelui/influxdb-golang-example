c66e39fa3f496969d516180dcfeaaa5718a9a1ed
nikolasmelui <slak@samaradom.ru>
2022-02-11 11:36:16 +0300

    refactor: change `panic` to `log.Fatal`

--- cmd/influxdb-golang-example.go
+++ cmd/influxdb-golang-example.go
@@ -4,20 +4,21 @@ import (
 	"fmt"
 	"github.com/NikolasMelui/influxdb-golang-example/internal/db"
 	"github.com/joho/godotenv"
+	"log"
 )
 
 func init() {
 	fmt.Println("Initializing the project...")
 	err := godotenv.Load("build/influxdb.env")
 	if err != nil {
-		panic(fmt.Errorf("cannot read the influxdb.env file"))
+		log.Fatal(fmt.Errorf("cannot read the influxdb.env file"))
 	}
 }
 
 func main() {
 	connection, err := db.ConnectToDB()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	fmt.Printf("Successfully connected to DB:\n")
 	fmt.Printf("%s", connection)
